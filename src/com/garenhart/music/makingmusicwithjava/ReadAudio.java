aa647a25001430fb15fd74219663c3f82cb638bf
Garen Hartunian <ghartunian@gmail.com>
2017-11-03 16:37:23 -0400

    Revert back to the original file to read.

--- src/com/garenhart/music/makingmusicwithjava/ReadAudio.java
+++ src/com/garenhart/music/makingmusicwithjava/ReadAudio.java
@@ -5,7 +5,7 @@ import jm.util.*;
 public class ReadAudio {
 
     public static void main(String[] args) {
-        float[] data = Read.audio("data/Jazz Improvisation L2 - F7 Altered.wav");
+        float[] data = Read.audio("data/welcome.au");
 
         for (int i=0; i<data.length; i++) {
             System.out.print(data[i] + " ");
